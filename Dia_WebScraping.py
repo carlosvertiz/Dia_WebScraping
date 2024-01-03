86861c2974b428f3496afbbdb4603de81ae6df2a
Carlos Vertiz <vertizcar@gmail.com>
2024-01-03 10:16:49 -0300

    script changes

--- Dia_WebScraping.py
+++ Dia_WebScraping.py
@@ -17,7 +17,7 @@ def cerrarVentanaSesion(driver):
     This function will close the windows that ask you to log in.
     """
     boton_presente = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'vtex-modal-layout-0-x-closeButtonContainer'))
+        EC.presence_of_element_located((By.CLASS_NAME, "vtex-modal-layout-0-x-closeButtonContainer"))
     )
     boton_presente.click()
     driver.implicitly_wait(10)
@@ -27,15 +27,19 @@ def scrollPage(driver):
     This function will scroll until the end of the page.
     """
 
-    # Obtain the actual page's height
-    height_before = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
+    # Obtain the actual page"s height
+    height_before = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight,"
+                                          " document.documentElement.clientHeight, document.documentElement.scrollHeight,"
+                                          " document.documentElement.offsetHeight );")
 
     # Scroll until the end of the page and wait the page complete load
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(2)
 
-    # Obtain the new page's height
-    height_after = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
+    # Obtain the new page"s height
+    height_after = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight,"
+                                         " document.documentElement.clientHeight, document.documentElement.scrollHeight,"
+                                         " document.documentElement.offsetHeight );")
 
     # Repeat the process until the hight dont change anymore
     while height_before < height_after:
@@ -43,7 +47,9 @@ def scrollPage(driver):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         driver.implicitly_wait(10)
         time.sleep(2)
-        height_after = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
+        height_after = driver.execute_script("return Math.max( document.body.scrollHeight,"
+                                             " document.body.offsetHeight, document.documentElement.clientHeight,"
+                                             " document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
     
 
 def openPage(url, content = True):
@@ -53,10 +59,10 @@ def openPage(url, content = True):
     """
     # Config chrome options. The window will open in full screen.
     chrome_options = Options()
-    chrome_options.add_argument('--start-maximized') 
+    chrome_options.add_argument("--start-maximized") 
 
-    #Chromedriver's path, if you dont have it, you can download it in chromedriver page.
-    chrome_driver_path = 'C:/Users/mega_/OneDrive/Desktop/chromedriver.exe'
+    #Chromedriver"s path, if you dont have it, you can download it in chromedriver page.
+    chrome_driver_path = "C:/Users/mega_/OneDrive/Desktop/chromedriver.exe"
     chrome_service = ChromeService(executable_path=chrome_driver_path)
 
     # Initialize chrome
@@ -67,7 +73,7 @@ def openPage(url, content = True):
     driver.implicitly_wait(10)
     time.sleep(2)
 
-    # if content return the page content, else, return page's driver
+    # if content return the page content, else, return page"s driver
     if content:
         page_content = driver.page_source
         driver.quit()
@@ -76,20 +82,20 @@ def openPage(url, content = True):
         return driver
     
 
-# Open Dia's main page and close the log in window
+# Open Dia"s main page and close the log in window
 driver = openPage("https://diaonline.supermercadosdia.com.ar/", False)
 driver.implicitly_wait(10)
 cerrarVentanaSesion(driver)
 
 #Open the categories tab.
-button_menu = driver.find_element(By.CSS_SELECTOR, '[data-id="mega-menu-trigger-button"]')
+button_menu = driver.find_element(By.CSS_SELECTOR, "[data-id='mega-menu-trigger-button']")
 button_menu.click()
 driver.implicitly_wait(10)
 time.sleep(2)
 
 # Seach for all the categories name.
 categories = WebDriverWait(driver, 10).until(
-    EC.presence_of_all_elements_located((By.CLASS_NAME, 'diaio-store-5-x-menuItem'))
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "diaio-store-5-x-menuItem"))
 )
 
 dict_category = {}
@@ -109,11 +115,12 @@ for category in categories:
 
     #Get page content
     page_content = driver.page_source
-    soup = BeautifulSoup(page_content, 'html.parser')
+    soup = BeautifulSoup(page_content, "html.parser")
 
     #find all sub_categories from the category
-    sub_categories = soup.find('ul', class_='diaio-store-5-x-menuContainer diaio-store-5-x-submenuContainer list ma0 pa0 pb3 br b--muted-4')
-    sub_categories = sub_categories.find_all('li', class_='diaio-store-5-x-menuItem')
+    sub_categories = soup.find("ul", class_="diaio-store-5-x-menuContainer diaio-store-5-x-"
+                               "submenuContainer list ma0 pa0 pb3 br b--muted-4")
+    sub_categories = sub_categories.find_all("li", class_="diaio-store-5-x-menuItem")
 
     #Iter over the sub categories and save then in a {category:[sub_categories]} dict                          
     for sub_category in sub_categories:
@@ -138,16 +145,18 @@ for key in dict_category.keys():
         
         # obtain page content and its products.
         page_content = driver.page_source
-        soup = BeautifulSoup(page_content, 'html.parser')
+        soup = BeautifulSoup(page_content, "html.parser")
         page_content_products = soup.find("div", id = "gallery-layout-container")
         if not page_content_products:
             driver.quit()
             continue
-        products = page_content_products.find_all("div", class_ = "vtex-search-result-3-x-galleryItem vtex-search-result-3-x-galleryItem--normal vtex-search-result-3-x-galleryItem--default pa4")
+        products = page_content_products.find_all("div", class_ = "vtex-search-result-3-x-galleryItem vtex-search-result"
+                                                  "-3-x-galleryItem--normal vtex-search-result-3-x-galleryItem--default pa4")
         # iter over the products, save its name and price in items_data.
         for product in products:
              
-            name = product.find("span", class_="vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body")
+            name = product.find("span", class_="vtex-product-summary-2-x-productBrand vtex-"
+                                "product-summary-2-x-brandName t-body")
             price = product.find("span", class_="vtex-product-price-1-x-currencyContainer")
             if price:
                 name = name.text.strip()
